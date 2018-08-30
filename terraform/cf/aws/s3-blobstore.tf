b26db7d5c1b408c946ef7e41550253274ca4b3b3
J Gregory <john@johngregory.me.uk>
2018-08-30 17:57:16 +0100

    reworking pipeline

--- terraform/cf/aws/s3-blobstore.tf
+++ terraform/cf/aws/s3-blobstore.tf
@@ -17,14 +17,14 @@ resource "aws_s3_bucket" "cf_buildpacks" {
     enabled = false
   }
 
-  # server_side_encryption_configuration {
-  #   rule {
-  #     apply_server_side_encryption_by_default {
-  #       kms_master_key_id = "${aws_kms_key.cf_blobstore_key.arn}"
-  #       sse_algorithm     = "aws:kms"
-  #     }
-  #   }
-  # }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "${aws_kms_key.cf_blobstore_key.arn}"
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
 
   tags {
     Name        = "${var.s3_prefix}-${var.environment}-cf-buildpacks"
